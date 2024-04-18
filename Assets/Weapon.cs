dfbab265363f2977ab523cea3c411e85e357c29a
jutae-s <144840717+jutae-s@users.noreply.github.com>
2024-04-18 21:37:42 +0200

    MegaGun

--- Assets/Weapon.cs
+++ Assets/Weapon.cs
@@ -22,26 +22,14 @@ public class Weapon : MonoBehaviour
     // Update is called once per frame
     public void Update()
     {
-        if(isGunTimerdone)
-        {
-            gunTimer += Time.deltaTime;
 
-        }
-        if(gunTimer >= 0.25f)
-        {
-            Movement player = GetComponent<Movement>();
-            player.isRailing(false);
-        }
        
        
         if (Input.GetMouseButtonDown(0))
         {
             weapon.Fire();
         }
-        if (Input.GetKeyDown(KeyCode.B))
-        {
-            Railgun();
-        }
+      
 
         mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
     }
@@ -54,30 +42,7 @@ public class Weapon : MonoBehaviour
         
 
     }
-    public void Railgun()
-    {
-        isGunTimerdone = true;
-        Movement player = GetComponent<Movement>();
-        
-        player.isRailing(true);
 
-
-        Matrix4x4 recoilMatrix = new Matrix4x4();
-        Vector4 recoil = new Vector4();
-        float angle = Mathf.Deg2Rad * Random.Range(-5f, 5f);
-        recoil.x = Mathf.Cos(angle);
-        recoil.y = Mathf.Sin(angle);
-        recoilMatrix[0, 0] = recoil.x;
-        recoilMatrix[0, 1] = -recoil.y;
-        recoilMatrix[1, 0] = recoil.y;
-        recoilMatrix[1, 1] = recoil.x;
-
-        Vector3 direction = recoilMatrix.MultiplyVector(firePoint.up);
-        rb.AddForce(-direction * 10, ForceMode2D.Impulse);
-        weapon.RailGunFire();
-       
-        
-    }
 
     
 }
